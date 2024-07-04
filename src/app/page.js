c64a8fe7abfe4da5916ddd610d1923c4bcb5ab37
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-07-04 09:56:14 +0600

    Hero Section done

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import HeroSection from "./components/HeroSection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div className="container mx-auto">
+      <div className="container mx-auto w-11/12">
         <HeroSection></HeroSection>
       </div>
     </main>

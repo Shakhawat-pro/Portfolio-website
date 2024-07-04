b36d9827423bdf121d99d0f1f216ecc811825bca
Shakhawat-pro <ratulhossain2692004@gmail.com>
2024-07-04 11:26:10 +0600

    Navbar Section done

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
     return (
         <div className='flex max-md:flex-col flex-row my-4 gap-14 '>
             <div className='col-span-7 place-self-center grid px-5 max-md:text-center space-y-5'>
-                <h1 className='text-transparent text-4xl lg:text-6xl font-extrabold bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Hello, I`m Shakhawat
+                <h1 className='text-transparent max-sm:text-2xl text-4xl lg:text-6xl font-extrabold bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Hello, I`m Shakhawat
                     <span>
                         <br />
                         <TypeAnimation
@@ -27,7 +27,7 @@ const HeroSection = () => {
                 </h1>
                 <p className='text-[#ADB7BE]'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur atque corrupti quaerat. Quidem, nobis earum.</p>
                 <div className='flex max-md:flex-col flex-row gap-5 max-md:mx-auto max-md:w-full'>
-                    <button className='btn max-md:w-full rounded-3xl text-white border-none  bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 transition-transform transform hover:scale-110'>Hire Me</button>
+                    <button className='px-6 py-3 max-md:w-full rounded-3xl text-white border-none  bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 transition-transform transform hover:scale-110'>Hire Me</button>
                     <div className='transition-transform transform hover:scale-110'>
                         <button className="w-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 px-1 py-1  text-white rounded-full">
                             <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2 ">
@@ -44,6 +44,8 @@ const HeroSection = () => {
                     className="absolute transform -translate-x-1/2  left-1/2"
                     width={200}
                     height={200}
+                    priority 
+                    style={{ width: 'auto', height: '100%' }} 
                 />
             </div>
         </div>
---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,46 @@
+'use client'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { MdClose, MdMenuOpen } from "react-icons/md";
+
+const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = useState(false)
+
+    const navLinks = <>
+        <li><Link href={"#about"} className='hover:text-white'>About</Link></li>
+        <li><Link href={"#about"} className='hover:text-white'>Projects</Link></li>
+        <li><Link href={"#about"} className='hover:text-white'>Contact</Link></li>
+    </>
+
+    return (
+        <nav className='fixed top-0 left-0 right-0 z-50 bg-[#121212] py-4 border-b-2'>
+            <div className='flex items-center mx-auto justify-between w-11/12'>
+                <Link href={"/"} className='text-white text-5xl font-bold uppercase'>Logo</Link>
+                {/* Burger Menu */}
+                <div className='sm:hidden text-white text-4xl'>
+                    {
+                        navbarOpen ? (
+                            <button className='transition-transform transform hover:scale-110' onClick={() => setNavbarOpen(false)}><MdClose /></button>
+                        ) :
+                            (
+                                <button className='transition-transform transform hover:scale-110' onClick={() => setNavbarOpen(true)}><MdMenuOpen /></button>
+                            )
+                    }
+                </div>
+                {/* large screen */}
+                <div className='menu max-sm:hidden'>
+                    <ul className='flex gap-5 text-xl text-slate-200 border-slate-200 '>
+                        {navLinks}
+                    </ul>
+                </div>
+            </div>
+            {navbarOpen ? <div>
+                    <ul className='flex flex-col mt-5 justify-center items-center gap-5 text-xl text-slate-200 border-slate-200 '>
+                        {navLinks}
+                    </ul>
+            </div> : null}
+        </nav>
+    )
+}
+
+export default Navbar
